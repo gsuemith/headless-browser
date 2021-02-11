e5b03feb107d83daf968aa9f1664ae2dc485ce7f
Garrick Suemith <garrick.suemith@gmail.com>
2021-02-11 17:42:11 -0500

    initial commit

---
+++ scrape.py
@@ -0,0 +1,5 @@
+from urllib.request import urlopen 
+from urllib.request import Request as uReq
+from bs4 import BeautifulSoup as soup
+
+rc_url = 'https://www.royalcaribbean.com/cruises/?country=USA&departureCode_BWI=true&departureCode_BYE=true&departureCode_FLL=true&departureCode_MIA=true&departureCode_PCN=true&departureCode_TPA=true'
